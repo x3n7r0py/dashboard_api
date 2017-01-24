f230087711315120dc7ec21c9370fec44f7c59ad
Loga <lmcniece@conversantmedia.com>
2017-01-24 16:54:52 -0600

    equities now pull from marketdata; cleans up yahoo query pull and handling

--- yahoo_api.py
+++ yahoo_api.py
@@ -8,97 +8,85 @@ import requests
 import psycopg2
 import connect
 import re
+import json
 from datetime import datetime, timedelta
 
 def yahoo_api():
 
 	while True:
 		try:
-			#init vars
-			data = []
-			stock_string = ""
-			
 			#Connect to database
+			now = datetime.now()
+			mktopen = now.replace(hour=9, minute=15, second=0, microsecond=0)
+			mktclose = now.replace(hour=15, minute=30, second=0, microsecond=0)
+			print("Time is "+str(now))
+			print ("Is market open? " + str(now > mktopen and now < mktclose))
+			if now < mktopen or now > mktclose:
+				print("Waiting one hour.....")
+				time.sleep(3600)
+				continue
 			conn = connect.heroku()
 			cursor = conn.cursor()
-
-			#Pull symbols from portfolio
-			cursor.execute("SELECT symbol FROM stock_info ORDER BY symbol asc;")
-			rows = cursor.fetchall()
-			for row in rows:
-				stock_string += str(row)
-			stock_string = re.sub(r'[()]?(,\)$)?',"",stock_string) #strip paraentheses and trailing comma
 			
-			#Pull stock info from Yahoo
+			#Define our connection string
+			#conn_string = "' dbname='' user='' password=''"
+			#conn = psycopg2.connect(conn_string)
+			#cursor = conn.cursor()
+			
+			
+			#Pull global stock and index info from Yahoo
+			cursor.execute("SELECT symbol FROM stock_info where type <> 'equity' ORDER BY symbol asc;")
+			symbols = ""
+			for row in cursor.fetchall():
+				symbols += "'"+row[0]+"',"         
+			symbols = re.sub(r',$',"",symbols)
 			yql = 'select symbol, LastTradeDate, LastTradeWithTime, Change, ChangeinPercent from yahoo.finance.quotes \
-			where symbol in ({})'.format(stock_string)
+			where symbol in ({})'.format(symbols)
 			yql = urllib.parse.quote(yql)
 			url = 'https://query.yahooapis.com/v1/public/yql?q=' + yql + '&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
 			response = requests.get(url)
-			data = response.json()
-			count = data['query']['count'] - 1
+			data = response.json()["query"]["results"]["quote"]
+			conn.close()
 
 			#Generate query and update database values
-			for i in data['query']['results']['quote']:
-				symbol = i['symbol'].lower()
-				price = re.search(r'<b>(.*)</b>', i['LastTradeWithTime']).group(1)
-				change = i['Change'].strip("%+")
-				change_per = i['ChangeinPercent'].strip("%+")
-				SQL = "update stock_info set current_price = " + price + ", \
-					last_updated = CURRENT_TIMESTAMP, change = " + change + ", \
-					change_perc = " + change_per + " where symbol = '" + symbol + "';"
+			for stock in data:
+				symbol = stock['symbol'].lower()
+				lastPrice = re.search(r'<b>(.*)</b>', stock['LastTradeWithTime']).group(1)
+				netChange = stock['Change'].strip("%+")
+				percentChange = stock['ChangeinPercent'].strip("%+")
+				SQL = "update stock_info set current_price = " + lastPrice + ", \
+					last_updated = CURRENT_TIMESTAMP, change = " + netChange + ", \
+					change_perc = " + percentChange + " where symbol = '" + symbol + "';"
+				print ("{} : {} : {} : {}".format(symbol, lastPrice, netChange, percentChange))		
+			
+			#Pull stock info from MarketData
+			cursor.execute("SELECT symbol FROM stock_info where type = 'equity' ORDER BY symbol asc;")
+			symbols = "";
+			for row in cursor.fetchall():
+				symbols += row[0]+","         
+			url = "http://marketdata.websol.barchart.com/getQuote.json?key=c220e3123aa026d0a8a8e958f9f1672a&symbols=" + symbols
+			response = requests.get(url)
+			data = response.json()['results']
+			for stock in data:
+				symbol = (stock["symbol"])
+				lastPrice = str((stock["lastPrice"]))
+				netChange = str((stock["netChange"]))
+				percentChange = str((stock["percentChange"]))
+				SQL = "update stock_info set current_price = " + lastPrice + ", \
+					last_updated = CURRENT_TIMESTAMP, change = " + netChange + ", \
+					change_perc = " + percentChange + " where symbol = '" + symbol + "';"
+				cursor.execute(SQL)
+				conn.commit()
+				print ("{} : {} : {} : {}".format(symbol, lastPrice, netChange, percentChange))
 
-				#Check if Yahoo API updated, else ignore
-				if re.search(r':',i['LastTradeWithTime']) != None:
-					update = int(re.search(r'(.*):', i['LastTradeWithTime']).group(1)) + 4 #convert EST -> GMT
-					now = (datetime.now() - timedelta(minutes=15)).hour
-					if now > 12: now -= 12 #convert 24 hr -> 12 hr
-					if update > 12: update -= 12
-					if update == now:
-						status = 1
-					else: #fallback to date check
-						update = datetime.strptime(i['LastTradeDate'], '%m/%d/%Y').strftime('%Y-%m-%d')
-						now = str(datetime.now().date())
-						if update == now:
-							status = 1
-						else:
-							status = 0
-				#if mutual fund perform only date check
-				elif re.search(r'x$', symbol) != None or datetime.now().hour > 15:
-					update = datetime.strptime(i['LastTradeDate'], '%m/%d/%Y').strftime('%Y-%m-%d')
-					now = str(datetime.now().date())
-					if update == now:
-						status = 1
-					else:
-						status = 0
-				else:
-					update = "null"
-					now = "null"
-					status = 0
-				if status == 1:
-					print ("\n{} updated".format(symbol.upper()))
-					print ("{} == {}".format(update, now))	
-					cursor.execute(SQL)
-					print ("psql : ", cursor.statusmessage)
-					conn.commit()
-				elif status == 0:
-					print ("\n{} not updated".format(symbol.upper()))
-					print ("{} != {}".format(update, now))
-				else: print ("ERROR - STATUS NULL")
-
-				print ("{} : {} : {}".format(price, change, change_per))
-
-					
-			conn.close()                    
 			print ("\nClosing Connection")
 			print ("Time =", datetime.now().time(), "\n\n")
-			time.sleep(20)
+			conn.close()
+			time.sleep(300)
 	
 		except:
 			print ("Unexpected error:", sys.exc_info())
-			print (i, "\n")
-			print ("Data:", data)
-			time.sleep(20)
+			time.sleep(300)
 			continue
 	 
 if __name__ == "__main__":
